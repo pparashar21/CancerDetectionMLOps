ce95a86cc16bedd527fb574c84446b0125ab3f9e
pparashar21 <prasoonparashar21@gmail.com>
2025-06-29 19:09:20 -0400

    Changes to reflect new data ingestion methodology in data_ingestion.py

--- CancerClassification/entity/config_entity.py
+++ CancerClassification/entity/config_entity.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass(frozen=True)
+@dataclass
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from CancerClassification import logger
+import sys
+from CancerClassification.utils.logger import logging
+from CancerClassification.utils.exception_handler import ExceptionHandler
 from CancerClassification.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
-    logger.info(f">>>>> Stage, {STAGE_NAME} started! <<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>> Stage, {STAGE_NAME} completed! <<<<<")
-    #check
+    logging.info(f">>>>> Stage, {STAGE_NAME} started! <<<<<")
+    # obj = DataIngestionTrainingPipeline()
+    # obj.main()
+    a = 1/0
+    logging.info(f">>>>> Stage, {STAGE_NAME} completed! <<<<<")
 
 except Exception as e:
-    logger.exception(e)
-    raise e
+    logging.exception(ExceptionHandler(e, sys))
