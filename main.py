21ebd01b8160e3dc98d73995c12bbba0bc6f3b09
pparashar21 <prasoonparashar21@gmail.com>
2025-07-13 10:04:24 -0400

    Ready for SageMaker training

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import sys
+from CancerClassification.config.configuration import configManager
+from CancerClassification.components.data_preparation import DataPreparation
+from CancerClassification.components.model_trainer import ModelTrainer
 from CancerClassification.utils.logger import logging
 from CancerClassification.utils.exception_handler import ExceptionHandler
-# from CancerClassification.pipeline.data_ingestion import DataIngestionTrainingPipeline
-
-STAGE_NAME = "Data Ingestion Stage"
 
 try:
-    logging.info(f">>>>> Stage, {STAGE_NAME} started! <<<<<")
-    # obj = DataIngestionTrainingPipeline()
-    # obj.main()
-    a = 1/0
-    logging.info(f">>>>> Stage, {STAGE_NAME} completed! <<<<<")
+    logging.info(">>>>>>>>>>>>>>>>>>>>Initialising config manager<<<<<<<<<<<<<<<<<<<<<<<<<")
+    c = configManager()
+    dpc = c.get_data_preparation_config()
+    dp = DataPreparation(dpc)
+
+    logging.info(">>>>>>>>>>>>>>>>>>>>Running Data Preparation<<<<<<<<<<<<<<<<<<<<<<<<<")
+    train, valid, test, class_names = dp.run()
+    print(class_names)
+
+    logging.info(">>>>>>>>>>>>>>>>>>>>Data Preparation done!<<<<<<<<<<<<<<<<<<<<<<<<<")
+    logging.info(">>>>>>>>>>>>>>>>>>>>Starting Model Building<<<<<<<<<<<<<<<<<<<<<<<<<")
+    trainer = ModelTrainer(train, valid, test, class_names)
+    logging.info(">>>>>>>>>>>>>>>>>>>>Model Architecture Created<<<<<<<<<<<<<<<<<<<<<<<<<")
+    logging.info(trainer.model.summary)
+    logging.info(">>>>>>>>>>>>>>>>>>>>Starting Model Training<<<<<<<<<<<<<<<<<<<<<<<<<")
+    trainer.run()
+
 
 except Exception as e:
     logging.exception(ExceptionHandler(e, sys))
